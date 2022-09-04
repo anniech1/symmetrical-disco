0549040f5588d1a8a57a7db855b3cbf3e07caceb
anniech1 <ach577@nyu.edu>
2022-09-04 01:38:20 -0400

    added const for questions, answers, and startQuiz, next Question, pickAnswer functions in js

---
+++ script.js
@@ -0,0 +1,115 @@
+const startButton = document.getElementById('startBtn')
+const questionBox = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answerBtns')
+const nextButton = document.getElementById('nextBtn')
+const questions = [
+    {
+        question: 'Which of the following are true about JavaScript?',
+        answers: [
+            {text: 'It is only a server-side language', correct: false},
+            {text: 'It is only a client-side language', correct: false},
+            {text: 'It is neither a server-side or client-side language', correct: false},
+            {text: 'It is both a server-side and client-side language', correct: true}
+        ]
+    },
+    {
+        question: 'What can you use JavaScript to do?',
+        answers: [
+            {text: 'You can use it to make web pages interactive', correct: true},
+            {text: 'You can use it to make breakfast', correct: false},
+            {text: 'You can put it in your coffee', correct: false},
+            {text: 'You can use it to fuel your car', correct: false}
+        ]
+    },
+    {
+        question: 'With arrays in Javascript you can do the following:',
+        answers: [
+            {text: 'Store other arrays', correct: false},
+            {text: 'Store strings and numbers', correct: false},
+            {text: 'Store booleans', correct: false},
+            {text: 'All of the above', correct: true}
+        ]
+    },
+    {
+        question: 'Which of the following are NOT a common data type in JavaScript?',
+        answers: [
+            {text: 'Booleans', correct: false},
+            {text: 'Prompts', correct: true},
+            {text: 'Strings', correct: false},
+            {text: 'Numbers', correct: false}
+        ]
+    }
+]
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', ()=>{
+    currentQuestionIndex++
+    nextQuestion()
+})
+
+function startQuiz (){
+    startButton.classList.add('hide')
+    questionBox.classList.remove('hide')
+    currentQuestionIndex = 0
+    nextQuestion()
+
+}
+
+function nextQuestion(){
+    resetQuestions()
+    displayQuestion([currentQuestionIndex])
+}
+
+function displayQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+          button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+      })
+}
+
+function resetQuestions() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+  
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('incorrect')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
